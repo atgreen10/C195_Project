201e978852d2957c8a25c07354f5be910fd024c0
atgreen10 <austin.green0210@gmail.com>
2021-02-20 23:48:17 -0600

    Appointment model

---
+++ src/model/Appointment.java
@@ -0,0 +1,103 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+
+    private int appointmentID;
+    private String title;
+    private String description;
+    private String location;
+    private String contact;
+    private String apptType;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private int customerID;
+    private int userID;
+
+    public Appointment(){};
+
+    public Appointment(int appointmentID, String title, String description, String location, String contact,
+                       String apptType, LocalDateTime startDateTime, LocalDateTime endDateTime, int customerID,
+                       int userID){};
+
+    public int getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(int appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getApptType() {
+        return apptType;
+    }
+
+    public void setApptType(String apptType) {
+        this.apptType = apptType;
+    }
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+}
